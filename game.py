f5ab6504ebf051ee0e2a285a546efae951776249
isk727 <isk727@gmail.com>
2024-04-05 12:52:03 +0900

    Add files via upload

--- game.py
+++ game.py
@@ -3,20 +3,8 @@ from datetime import datetime
 import json
 
 class GameClass:
-
-#st_rb = 0
-#rbCount = 0
-#rb_sig_timer = 0
-
-#st_bb = 0
-#bbCount = 0
-#bb_sig_timer = 0
-
-
   global config_ini
   global DNS
-#  global gkey
-#  gkey = ""
   global jsonkey
   global RP_ID
   global userid
@@ -26,33 +14,43 @@ class GameClass:
   global ct_rb
   global start_at
   global stop_at
-#  global json_file
-#  json_file = ""
+#  global start_point
+#  global end
 
-  def __init__(self, name, json_file):
-    self.name = name
+  def __init__(self, json_file):
+#    self.name = name
     self.json_file = json_file
 #    self.name = config_ini['DEFAULT']['User']
 
   def __del__(self):
     print('インスタンスが破棄されました')
  
-  def start(self, gkey):
-    self.ct_rb = 0
-    self.ct_bb = 0
+  def start(self, gkey, point):
     self.gkey = gkey
+    self.start_pt = int(point)
+    self.stop_pt = 0
+    self.point = 0
+    self.credit = int(point)#0
+    self.ct_rb = 0
+    self.st_rb = 0
+    self.rbCount = 0
+    self.ct_bb = 0
+    self.st_bb = 0
+    self.bbCount = 0
     self.start_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     self.stop_at = ""
 
   def stop(self, gkey):
+    self.stop_pt = 10
     #self.ct_rb = 45
     #self.ct_bb = 12345
     self.gkey = gkey
+    self.stop_pt = self.credit
     self.stop_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-  def my_method(self):
+#  def my_method(self):
 #    print("クラスの名前は「" + self.name + "」です。" )
-    return self.name
+#    return self.name
 
 #  @property
   def json_dump(self):
@@ -60,9 +58,14 @@ class GameClass:
 #    cwd = os.getcwd()
     dict = {
       "gkey": self.gkey,
-      "rb": self.ct_rb,
-      "bb": self.ct_bb,
-      "ccc": self.name,
+      "start_pt": int(self.start_pt),
+      "stop_pt": int(self.stop_pt),
+      "rb": int(self.ct_rb),
+      "rb2": int(self.rbCount),
+      "bb": int(self.ct_bb),
+      "credit": int(self.credit),
+      "point": int(self.point),
+#      "ccc": self.name,
 #      "cwd": cwd,
       "jsonpath" : self.json_file,
       "start_at": self.start_at,
