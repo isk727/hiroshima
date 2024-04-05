e1c94e2baf03c8afdc379b92a66101f5f17a582c
isk727 <isk727@gmail.com>
2024-04-06 08:33:13 +0900

    Update script.py

--- script.py
+++ script.py
@@ -123,10 +123,9 @@ def set_point(n):
 # --------------------------------
 def clear():
   global config_ini
-  if (int(e) > 0):
-    GPIO.output(int(config_ini['GPI0']['CLEAR1']), GPIO.HIGH)
-    time.sleep(0.1)
-    GPIO.output(int(config_ini['GPI0']['CLEAR2']), GPIO.HIGH)
+  GPIO.output(int(config_ini['GPI0']['CLEAR1']), GPIO.HIGH)
+  time.sleep(0.1)
+  GPIO.output(int(config_ini['GPI0']['CLEAR2']), GPIO.HIGH)
 
 # ################################
 def destroy():
